a2d00bdb092d332b5277c6d2c7fc599b9267e8b8
gmeikulis <gatis.meikulis@gmail.com>
2025-09-29 00:58:08 +0300

    few moves demo play

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 import random
 
+from backend.application.game_command_factory import GameCommandFactory
+from backend.application.game_table_manager import GameTableManager
 from backend.domain.core.user_id import UserId
 from backend.domain.game.five_hundred.domain.constants import BID_STEP, MAX_BID, MIN_BID
 from backend.domain.game.five_hundred.domain.five_hundred_command import (
@@ -12,6 +14,7 @@ from backend.domain.game.five_hundred.domain.five_hundred_event import FiveHundr
 from backend.domain.game.five_hundred.domain.five_hundred_game import FiveHundredGame
 from backend.domain.game.five_hundred.domain.five_hundred_phase import FiveHundredPhase
 from backend.domain.game.game_name import GameName
+from backend.domain.lobby.lobby import Lobby
 from backend.domain.table.game_table import GameTable
 from backend.domain.table.game_table_config import GameTableConfig
 from backend.domain.table.game_table_factory import GameTableFactory
@@ -26,10 +29,12 @@ fivehundred_table_1 = GameTableFactory.create(
     game_name=GameName.FIVE_HUNDRED,
 )
 
+user_id_1 = UserId.generate()
+
+fivehundred_table_1.add_player(user_id=user_id_1)
 fivehundred_table_1.add_player(user_id=UserId.generate())
 fivehundred_table_1.add_player(user_id=UserId.generate())
-fivehundred_table_1.add_player(user_id=UserId.generate())
-fivehundred_table_1.start_game()
+fivehundred_table_1.start_game(user_id_1)
 
 
 def auto_player(
@@ -76,7 +81,7 @@ def auto_player(
                     else:
                         cmd = MakeBidCommand(bid=random.choice(range(highest_bid, 120 + 1, BID_STEP)))
 
-                    events = table.process_command(active_uid, cmd)
+                    events = table.play_turn(active_uid, cmd)
 
                     all_events.extend(events)
                     iterations = 0
@@ -92,7 +97,7 @@ def auto_player(
                     card1, card2 = random.sample(active_seats_cards, 2)
                     cmd = PassCardsCommand(card_to_next_seat=card1, card_to_prev_seat=card2)
 
-                    events = table.process_command(active_uid, cmd)
+                    events = table.play_turn(active_uid, cmd)
 
                     all_events.extend(events)
                     iterations = 0
@@ -111,7 +116,7 @@ def auto_player(
                     card_to_play = random.choice(cards_allowed_to_play)
                     cmd = PlayCardCommand(card=card_to_play)
 
-                    events = table.process_command(active_uid, cmd)
+                    events = table.play_turn(active_uid, cmd)
 
                     all_events.extend(events)
                     iterations = 0
@@ -128,5 +133,42 @@ def auto_player(
 
 
 # Example: run the auto player to completion and print the number of events
-events = auto_player(fivehundred_table_1)
-print(f"Auto play produced {len(events)} events")
+# events = auto_player(fivehundred_table_1)
+# print(f"Auto play produced {len(events)} events")
+
+
+game_command_factory = GameCommandFactory()
+lobby = Lobby()
+game_table_manager = GameTableManager(lobby, game_command_factory)
+
+game_table_manager.add_table(
+    GameName.FIVE_HUNDRED,
+    GameTableConfig(
+        game_name=GameName.FIVE_HUNDRED,
+        min_players=3,
+        max_players=3,
+        automatic_start=True,
+    ),
+)
+
+tables = game_table_manager.get_tables()
+
+table_id = tables[0].id
+
+user_id_1 = UserId.generate()
+user_id_2 = UserId.generate()
+user_id_3 = UserId.generate()
+
+
+game_table_manager.join_table(table_id, user_id_1, seat_number=1)
+game_table_manager.join_table(table_id, user_id_2, seat_number=2)
+game_table_manager.join_table(table_id, user_id_3, seat_number=3)
+print(game_table_manager.get_tables()[0])
+game_table_manager.start_game(table_id, user_id_1)
+print(game_table_manager.get_tables()[0])
+
+
+game_table_manager.play_turn(table_id, user_id_1, {"type": "make_bid", "bid": 100})
+print(game_table_manager.get_tables()[0])
+game_table_manager.play_turn(table_id, user_id_2, None)
+print(game_table_manager.get_tables()[0])
