fcc8d45defd5c432380d73f769d648155890b43b
gmeikulis <gatis.meikulis@gmail.com>
2025-10-17 12:05:46 +0300

    refactor comment

--- backend/apps/gametables/application/igame_play_event_repository.py
+++ backend/apps/gametables/application/igame_play_event_repository.py
@@ -7,11 +7,7 @@ from game.common.game_event import GameEvent
 class IGamePlayEventRepository(Protocol):
     def append(self, table_id: str, game_events: Sequence[GameEvent]) -> None: ...
 
+    # Returns series of game-events limited by provided sequence numbers (inclusive) sorted by sequence number ascending
     def find_many(
         self, table_id: str, start_inclusive: int | None = None, end_inclusive: int | None = None
     ) -> Sequence[GameEvent]: ...
-
-    """
-    Returns:
-        series of game-events limited by provided sequence numbers (inclusive) sorted by sequence number ascending
-    """
