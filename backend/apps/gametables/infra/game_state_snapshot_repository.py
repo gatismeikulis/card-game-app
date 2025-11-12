425810b6dc5e3ee6c506d5550bcac19334d14015
gmeikulis <gatis.meikulis@gmail.com>
2025-11-12 21:08:35 +0200

    refactor snapshot repo

--- backend/apps/gametables/infra/game_state_snapshot_repository.py
+++ backend/apps/gametables/infra/game_state_snapshot_repository.py
@@ -27,19 +27,13 @@ class GameStateSnapshotRepository(IGameStateSnapshotRepository):
             sets.append("index:event_number:0")
 
         # Get intersection of all sets (keys that match all criteria)
-        keys = self.redis.sinter(*sets)
+        keys = self.redis.sinter(sets)
 
-        if not keys or len(keys) == 0:
+        if not keys:
             return None
 
-        key = keys.pop()  # TODO: fix this because this can return different snapshot if searched by turn_number (pop returns random element from set)
-
-        # if isinstance(key, bytes):
-        #     key = key.decode()
-        # elif not isinstance(key, str):
-        #     key = str(key)
-
-        print("key: ", key)
+        # in case filtering by turn_number, there can be multiple snapshots with the same turn_number, so we need to find the one with the smallest event_number
+        key = min(keys, key=lambda k: int(int(k.decode().rsplit(":", 1)[-1])))
 
         snapshot = self.redis.get(key)
 
@@ -54,9 +48,12 @@ class GameStateSnapshotRepository(IGameStateSnapshotRepository):
         with self.redis.pipeline(transaction=False) as pipe:
             for snap in snapshots:
                 key = self._make_key(snap["table_id"], snap["turn_number"], snap["event_number"])
-                _ = self.redis.set(key, json.dumps(snap["payload"]), ex=self.ttl_in_seconds)
+                _ = pipe.set(key, json.dumps(snap["payload"]), ex=self.ttl_in_seconds)
                 _ = pipe.sadd(f"index:table_id:{snap['table_id']}", key)
                 _ = pipe.sadd(f"index:turn_number:{snap['turn_number']}", key)
                 _ = pipe.sadd(f"index:event_number:{snap['event_number']}", key)
+                _ = pipe.expire(f"index:table_id:{snap['table_id']}", self.ttl_in_seconds)
+                _ = pipe.expire(f"index:turn_number:{snap['turn_number']}", self.ttl_in_seconds)
+                _ = pipe.expire(f"index:event_number:{snap['event_number']}", self.ttl_in_seconds)
 
             _ = pipe.execute()
